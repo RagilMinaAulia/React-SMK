9f4ddbe27b10702feec64b9f0c0937b66679a965
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-03-17 15:38:05 +0700

    Menambahkan Gambar

--- react-yt/src/pages/Sejarah.js
+++ react-yt/src/pages/Sejarah.js
@@ -1,8 +1,10 @@
+import logo1 from './logo1.png';
 function Sejarah() {
     return (
       <div className="App">
         <h1>Sejarah SMKN 2 Buduran</h1>
         <p>Isi sejarah SMKN 2 Bdr</p>
+        <img src={logo1} alt="logo" />
       </div>
     );
   }
