cd07a564eebc690902b913aa743525c229c1d523
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-03-17 15:19:33 +0700

    Instalasi React JS

---
+++ react-yt/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bismillah</h1>
+      <p>Dipermudah belajar react lancar</p>
+    </div>
+  );
+}
+
+export default App;
